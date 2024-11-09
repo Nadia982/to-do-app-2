fb2a183817a79769d63d5d144022db2f4c236c8e
Natalie Gillam <ngillam@hotmail.co.uk>
2024-11-09 23:49:27 +0000

    added getTodoHTML function

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ const addButton = $(".add-button");
 const todos = $(".todos");
 let todosJson = JSON.parse(localStorage.getItem("todos")) || [];
 
+function getTodoHTML(todo, index){
+    let checked = todo.status === "completed"? "checked" : "";
+    return `
+    <li class="todo>
+    <label for="${index}">
+        <input id="${index}" onclick="updateStatus(this) type="checkbox" ${checked}>
+        <span class="checked">${todo.name}</span>  
+    </label>
+    <button onclick="deleteTodo(this) class="delete-btn" data-index="${index}><i class="fa fa-times></i></button>
+    </li>
+    `
+}
